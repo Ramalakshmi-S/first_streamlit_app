caa64f6276cdf080258b732503212fb70b879429
Ramalakshmi-S <130063088+Ramalakshmi-S@users.noreply.github.com>
2023-04-06 18:09:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,8 @@
 import streamlit
 
 streamlit.title('Rama- New app')
-streamlit.header('Family')
-streamlit.text('Murugappan')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑🍞 Avocado Toast')
